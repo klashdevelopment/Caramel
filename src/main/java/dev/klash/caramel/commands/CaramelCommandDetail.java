f34975b62f630758a29ef1a57f79c993d6fdfc51
GavinGoGaming <57292172+GavinGoGaming@users.noreply.github.com>
2024-07-07 14:25:34 -0700

    Hotfix

--- src/main/java/dev/klash/caramel/commands/CaramelCommandDetail.java
+++ src/main/java/dev/klash/caramel/commands/CaramelCommandDetail.java
@@ -3,5 +3,7 @@ package dev.klash.caramel.commands;
 import org.bukkit.plugin.Plugin;
 
 public record CaramelCommandDetail(String label, String permission, Plugin plugin, String... aliases) {
-
+    public CaramelCommandDetail(String label, String permission, Plugin plugin) {
+        this(label, permission, plugin, new String[0]);
+    }
 }
